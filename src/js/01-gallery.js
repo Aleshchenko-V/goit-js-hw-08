b89882fea38c2d9b920375e0e02afdeebb926df2
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-14 19:55:52 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryListItems = createGallery(galleryItems);
+
+function createGallery(collection) {
+  return collection
+    .map(
+      ({ preview, original, description }) =>
+        `
+					<a class="gallery__item" href="${original}">
+						<img class="gallery__image" src="${preview}" alt="${description}" />
+					</a>
+				`
+    )
+    .join('');
+}
+
+function addGalleryToPage(el) {
+  galleryEl.insertAdjacentHTML('beforeend', el);
+}
+
+addGalleryToPage(galleryListItems);
+
+galleryEl.addEventListener('click', e => {
+  e.preventDefault();
+  const isGalleryItem = e.target.classList.contains('gallery__image');
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+});
