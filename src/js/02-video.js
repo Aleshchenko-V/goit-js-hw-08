6d5462fb45bab51c99daa3a7fec95279c9ce4e32
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-15 19:46:11 +0300

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
 import Vimeo from '@vimeo/player';
-
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formValues = {
+  email: '',
+  message: '',
+};
+
+document.addEventListener('DOMContentLoaded', setFormFields);
+form.addEventListener('input', throttle(onInputHandler, 500));
+form.addEventListener('submit', onSubmitHandler);
+
+function onInputHandler(e) {
+  formValues[e.target.name] = e.target.value;
+  const stringifyData = JSON.stringify(formValues);
+  localStorage.setItem(LOCALSTORAGE_KEY, stringifyData);
+}
+
+function setFormFields() {
+  try {
+    const cachedValues = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    const { email, message } = cachedValues;
+    console.log(email, message);
+    input.value = email;
+    textarea.value = message;
+  } catch (e) {
+    input.value = formValues.email;
+    textarea.value = formValues.message;
+  }
+}
+
+function onSubmitHandler(e) {
+  e.preventDefault();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  e.currentTarget.reset();
+  console.log(formValues);
+}
