f81e3b8c7cdadfbb6a803c83b5cdd5250aa3ccb4
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-15 11:57:22 +0300

    task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+localStorage.setItem(
+  LOCALSTORAGE_KEY,
+  localStorage.getItem(LOCALSTORAGE_KEY) || 0
+);
+
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+  }, 1000)
+);
+
+document.addEventListener(
+  'DOMContentLoaded0',
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+);
